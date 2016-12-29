ea5978f3e2832704ac4b6068056cd705466a14f6
Dan Book <grinnz@grinnz.com>
2016-12-28 22:11:59 -0500

    Bump version

--- lib/Mojolicious/Plugin/Subprocess.pm
+++ lib/Mojolicious/Plugin/Subprocess.pm
@@ -4,7 +4,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::IOLoop::Subprocess;
 use Scalar::Util 'weaken';
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 sub register {
   my ($self, $app, $options) = @_;
