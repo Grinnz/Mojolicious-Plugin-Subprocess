34c1eb53e25a7402bf7504189fb13ed8e5106233
Dan Book <grinnz@grinnz.com>
2017-06-08 02:27:26 -0400

    Bump version

--- lib/Mojolicious/Plugin/Subprocess.pm
+++ lib/Mojolicious/Plugin/Subprocess.pm
@@ -4,7 +4,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::IOLoop::Subprocess;
 use Scalar::Util 'weaken';
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 my ($deserialize, $serialize);
 
